e635ad3feffd62e766b3bda249390871cf9b7dc2
nazarpunk <nazarpunk@ya.ru>
2015-08-22 12:50:54 +0300

    current copy

---
+++ js/char.js
@@ -0,0 +1,235 @@
+function rand(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+get_tooltip_content = function(c){
+	c = "<div class='corner_top'></div><div class='corner_bottom'></div>"+c
+	return c
+}
+
+
+
+if (typeof ch === "undefined") alert("char_setting.js is corrupted")
+char = {}
+
+ch.draw = function(){
+	var out=""
+	//race
+	$.each(ch.set.race,function(k,v){
+		var title = (typeof v.title !== "undefined") ? v.title : k 
+		out+="<input id='race_"+k+"' type='radio' name='race' value='"+k+"'>\
+		<label for='race_"+k+"'><i></i><span>"+title+"</span></label>"
+	})
+	var l = $("#race_wrap").html($(out)).find("input[type=radio]").length
+	
+	if ($("input[type=radio]:checked","#race_wrap").length <= 0) $("input[type=radio]","#race_wrap").eq(rand(0,l-1)).prop("checked",true) 
+	
+	//sex
+	out=""
+	$.each(ch.set.sex, function(k,v){
+		var title = (typeof v.title !== "undefined") ? v.title : k 
+		out+="<input id='sex_"+k+"' type='radio' name='sex' value='"+k+"'>\
+		<label for='sex_"+k+"'>"+title+"</label>"		
+	})
+	var l = $("#sex_wrap").html($(out)).find("input[type=radio]").length
+	if ($("input[type=radio]:checked","#sex_wrap").length <= 0) $("input[type=radio]","#sex_wrap").eq(rand(0,l-1)).prop("checked",true) 
+
+	//skill
+	out=""
+	var elem = $("#skill_wrap")
+	var groups = {}
+
+	$.each(ch.set.skill,function(k,v){
+
+		var group = "all"
+		var group_name = "Хня"
+		if (typeof v.group !== "undefined") {
+			group =  v.group
+			group_name = group
+			
+			if (typeof ch.set.skill_group[v.group] !== "undefined")
+				if (typeof ch.set.skill_group[v.group].title !== "undefined")
+				group_name = ch.set.skill_group[v.group].title
+		}
+ 
+		
+		var title = (typeof v.title !== "undefined") ? v.title : k
+		
+		if (typeof groups[group] === "undefined")
+			groups[group] = $("<table><tr><th>"+group_name+"</th></tr></table>").addClass("skill_group").appendTo(elem)
+		
+		var val = -1
+		var tr = $("<tr><td>"+title+"</td><td class='skill_val'>"+val+"</td></tr>")
+			.attr("id","skill_"+k)
+			.addClass("skill")
+			.data("val",val)
+			.appendTo(groups[group])
+	})
+	
+	
+	
+}
+
+ch.portrait_draw = function(){
+	var race = ch.race_get()
+	var sex = ch.sex_get()
+	$("#portrait_img").html("<img src='img/portrait/"+ race + "_"+sex+".jpg' alt='' />")
+}
+
+ch.race_change = function(){
+	var race = ch.race_get()
+	
+	ch.portrait_draw()
+	ch.hitbox.draw()
+}
+ch.race_get = function() {
+	return  char.race = $("input[name=race]:checked", "#race_wrap").val()
+}
+ch.sex_change = function(){
+	ch.portrait_draw()
+}
+ch.sex_get = function(){
+	return char.sex = $("input[name=sex]:checked", "#sex_wrap").val()
+}
+
+ch.hitbox = {}
+char.hit = {}
+ch.hitbox.draw = function(){
+	var elem = $("#hitbox")
+	var race = ch.race_get()
+	var race =  (typeof ch.set.race[race].hit !== "undefined") ? ch.set.race[race].hit : {}
+	var def = $.extend({},ch.set.hit, race)
+	var line = 0, row = 0, line_tmp = 0
+	
+	$.each(def,function(k,v){
+		$.each(v,function(k1,v1){
+			line_tmp++
+		})
+		if (line_tmp > line) line = line_tmp
+		line_tmp = 0
+		row++
+	})
+	
+	var out = ""
+	for (var i = 1; i <= row; i++){ //i - line, k - row
+		var tr = ""
+		for (var k = 0; k < line; k++){
+			var t = "", c = ""
+			if ((typeof def[i] !== "undefined") & (typeof def[i][k] !== "undefined")) {
+				t = def[i][k]
+				var anim = ""
+				if (typeof char.hit[i] !== "undefined")
+					if (typeof char.hit[i][k] !== "undefined")
+						if (char.hit[i][k] != t)
+							var anim = "anim"
+					
+				c = "class='issue "+anim+"'"
+			}
+			
+			tr+="<td "+c+">"+t+"</td>"
+		}
+		out="<tr>"+tr+"</tr>"+out
+	}
+	char.hit = def
+	elem.html($(out))
+}
+
+
+$(function(){
+	
+	ch.draw()
+	ch.portrait_draw()
+	ch.hitbox.draw()
+	
+	
+	$("input[name=race]","#race_wrap").change(function(){ ch.race_change() })
+	$("input[name=sex]","#sex_wrap").change(function(){ ch.sex_change() })
+	
+	$(window).bind("mousemove",function(e){
+		var x = e.pageX
+		var w = $("#tooltip_wrap").width()
+		var of = -10
+		var cl = ""
+		
+		if (x-100 < w) {
+			cl = "right"
+			of = 10
+		} else {
+			var cl = "left"
+			of = -10
+		}
+		$("#tooltip_point").removeClass("left right").addClass(cl).css({
+			"top": e.pageY -10,
+			"left": x + of
+		})
+		
+	})
+
+	$(".skill", "#skill_wrap").hover(function(){
+		$("#tooltip_content").html($(this).attr("id") + " - "+ $(this).data("val"))
+		$("#tooltip_point").stop(true,true).fadeIn()
+		
+	}, function(){
+		$("#tooltip_point").delay(100).fadeOut("fast")
+		console.log(char)
+	})
+	
+	
+	
+	/*
+	
+
+	char.stat.draw = function(){
+		var elem = $("#stat")
+
+		$("#stat_wrap > table").empty()
+		
+		$.each (char.stat.name,function(k,v){
+			var val = -1
+			var tip = ""
+
+			var race = char.race.get()
+
+			if (typeof char.stat.val.def[k] !== "undefined"){
+				val = char.stat.val.def[k]
+				char.stat.current.base[k] = char.stat.val.def[k]
+				tip += " data-base='"+char.stat.val.def[k]+"'"
+			}		
+			
+			if (typeof char.stat.val[race] !== "undefined")
+			if(typeof char.stat.val[race][k] !== "undefined"){
+				val += char.stat.val[race][k]
+				char.stat.current.race[k] = char.stat.val[race][k]
+				tip += "data-race='"+char.stat.current.race[k]+"'"
+			}
+			tip += " data-val='"+val+"'"
+			
+			
+			
+			var cl = ""
+			if (val < 0) cl = "style='color:rgb(80,80,80)'"
+			if (val <= 0) val = ""
+			
+			var name = (v instanceof Array) ? v[0] : v
+			var append_to = "#stat_all"
+				
+			if ($.inArray(k,group_damage) >= 0 ) append_to = "#stat_attack"
+			if ($.inArray(k,group_stand) >= 0 ) append_to = "#stat_stand"
+			if ($.inArray(k,group_defence) >= 0 ) append_to = "#stat_defence"
+				console.log($.inArray(k,group_damage) )
+			
+			$("<tr "+tip+" "+cl+" ><td>"+name+"</td><td>"+val+"</td></tr>")
+				.data("stat",k)
+				.addClass("stat")
+				.appendTo($(append_to))
+		})
+		
+
+	}
+	
+	char.point.draw = function(){
+		$("#buy_point").html(char.point.def)
+	}
+
+*/
+});
--- js/char_setting.js
+++ js/char_setting.js
@@ -1,4 +1,4 @@
-﻿if (typeof ch === "undefined") ch = {}
+if (typeof ch === "undefined") ch = {}
 
 ch.set = {
 	race: {
@@ -8,11 +8,11 @@ ch.set = {
 		elf: {
 			title: "Эльф",
 			skill: {
-				aim_attack_melee : 1,
-				aim_attack_range : 1,
-				evasion : 1, 
-				avoid : 1,
-				hide : 1
+				aim_attack_melee: 1,
+				aim_attack_range: 1,
+				evasion: 1,
+				avoid: 1,
+				hide: 1
 			},
 		},
 		orc: {
@@ -20,12 +20,12 @@ ch.set = {
 			hit: {
 				1: [2, 2, 2, 2, 2, 2]
 			},
-			skill : {
-				power_attack : 1,
-				power_throw : 1,
-				swim : 1,
-				run : 1,
-				jump : 1
+			skill: {
+				power_attack: 1,
+				power_throw: 1,
+				swim: 1,
+				run: 1,
+				jump: 1
 			},
 		},
 		troll: {
@@ -33,14 +33,14 @@ ch.set = {
 			hit: {
 				1: [2, 2, 2, 1, 1, 1]
 			},
-			skill : {
-				aim_attack_melee : 1,
-				aim_attack_range : 1,
-				swim : 1,
-				run : 1,
-				jump : 1,
-				evasion : 1, 
-				avoid : 1
+			skill: {
+				aim_attack_melee: 1,
+				aim_attack_range: 1,
+				swim: 1,
+				run: 1,
+				jump: 1,
+				evasion: 1,
+				avoid: 1
 			},
 		},
 		dwarf: {
@@ -52,8 +52,8 @@ ch.set = {
 				4: [5, 5, 5],
 				5: [6, 6]
 			},
-			skill : {
-				avoid : 1
+			skill: {
+				avoid: 1
 			}
 		}
 	},
@@ -73,179 +73,177 @@ ch.set = {
 			title: "Самка"
 		}
 	},
-	skill_group : {
-		attack : {
-			title : "Атака"
+	skill_group: {
+		attack: {
+			title: "Атака"
 		},
-		defence : {
-			title : "Защита"
+		defence: {
+			title: "Защита"
 		}
 	},
 	skill: {
 		attack: {
 			title: "Атака",
-			group : "attack",
+			group: "attack",
 			def: 0, //default
 			desription: "Базовое агрессивное действие."
 		},
 		power_attack: {
 			title: "Мощная атака",
-			group : "attack",
+			group: "attack",
 			description: "Плюс к количеству урона. Мощный выстрел - только ростовые луки."
 		},
 		power_throw: {
 			title: "Мощный бросок",
-			group : "attack",
+			group: "attack",
 			description: "Бросок изо всех сил."
 		},
 		aim_attack_melee: {
 			title: "Точная атака",
-			group : "attack",
+			group: "attack",
 			description: "Плюс к шансу попадания по уязвимому месту."
 		},
 		aim_attack_range: {
 			title: "Точный выстрел",
-			group : "attack",
+			group: "attack",
 			description: "Всадить стрелу в глаз."
 		},
 		aim_attack_throw: {
 			title: "Точный бросок",
-			group : "attack",
+			group: "attack",
 			description: "Щвырнуть лаптем в очко."
 		},
 		splash_attack: {
 			title: "Круговая атака",
-			group : "attack",
+			group: "attack",
 			description: "Размах сплеча."
 		},
 		splash_attack_range: {
 			title: "Множественный выстрел",
-			group : "attack",
+			group: "attack",
 			description: "Всадить несколько стрел сразу."
 		},
 		splash_attack_throw: {
 			title: "Множественный бросок",
-			group : "attack",
+			group: "attack",
 			description: "Швырнуть охапку камней."
 		},
 		speed_attack: {
 			title: "Быстрая атака",
-			group : "attack",
+			group: "attack",
 			description: " Дополнительная базовая атака в ход. Стоит 2 очка. Максимум три атаки."
 		},
 		push: {
 			title: "Отброс",
-			group : "attack",
+			group: "attack",
 			description: "Сбивание с ног. Пинком. В табло. Отброс идёт как Быстрая атака."
 		},
 		shield_attack: {
 			title: "Удар щитом",
-			group : "attack",
+			group: "attack",
 			description: "Возможность наносить щитом различные атаки."
 		},
 		rush: {
 			title: "Натиск",
-			group : "attack",
+			group: "attack",
 			description: " Бег в выбранной стойке. Без штрафов к расстоянию."
 		},
 		provocation: {
 			title: "Провокация",
-			group : "attack",
+			group: "attack",
 			description: "Поддавшаяся цель атакует с штрафом на попадание. Штраф равен уровню провокации."
 		},
 		disarm: {
 			title: "Обезоруживание",
-			group : "attack",
+			group: "attack",
 			description: "Кэп"
 		},
 		feint: {
 			title: "Финт",
-			group : "attack",
+			group: "attack",
 			description: "Финт сбрасывает стойку цели. 1d6+ пойнты силы против 1d6 врага."
 		},
 
 		stance_defence: {
 			title: "Защитная стойка",
-			group : "defence",
+			group: "defence",
 			description: "Уровень защитной стойки = Бонус ко всем видам защиты."
 		},
 		stance_attack: {
 			title: "Атакующая стойка",
-			group : "defence",
+			group: "defence",
 			description: "Уровень= Бонус ко всем видам атаки."
 		},
 		stance_range: {
 			title: "Стойка Стрелка",
-			group : "defence",
+			group: "defence",
 			description: "Уровень= Бонус к попаданию у атак."
 		},
 		stance_range_move: {
 			title: "Стрельба в движении",
-			group : "defence",
+			group: "defence",
 			description: "Уровень=Бонус к попаданию при движении."
 		},
 		stance_magick: {
 			title: "Стойка Колдуна",
-			group : "defence",
+			group: "defence",
 			description: "Уровень= Бонус к выбранному знаку в заклинании."
 		},
 		stance_magik_move: {
 			title: "Колдовство в движении",
-			group : "defence",
+			group: "defence",
 			description: "Однократный неразвивающийся навык стоимостью в 5 пойнтов."
 		},
 		stance_agility: {
 			title: "Стойка ловкача",
-			group : "defence",
+			group: "defence",
 			description: "Уровень= Бонус к уклонению и избежанию."
 		},
 		aim: {
 			title: "Прицеливание",
-			group : "defence",
-			description: "Уровень= бонус к попаданию. Прицеливание накапливается, если не атаковать, и если цель не пропадает из виду. Прицеливание можно делать с оружием  ближнего боя. Прицеливание отключает бег."
+			group: "defence",
+			description: "Уровень= бонус к попаданию. Прицеливание накапливается, если не атаковать, и если цель не пропадает из виду. Прицеливание можно делать с оружием ближнего боя. Прицеливание отключает бег."
 		},
-
 
 		defence: {
 			title: " Ношение доспеха",
-			group : "defence",
+			group: "defence",
 			description: ""
 		},
 		block_shield: {
 			title: "Блокирование",
-			group : "defence",
+			group: "defence",
 			description: "Защита щитом. Щит не повреждается при успехе. Неудача - щит получает полный урон."
 		},
 		parry: {
 			title: "Парирование",
-			group : "defence",
+			group: "defence",
 			description: "Защита оружием. Оружие не повреждается при успехе. Неудача - оружие получает половину урона."
 		},
 		block_any: {
 			title: "Закрыться",
-			group : "defence",
+			group: "defence",
 			description: "Защита чем попало. \"Что попало\" всегда получает полный урон."
 		},
 		dodge: {
 			title: "Уворот",
-			group : "defence",
+			group: "defence",
 			description: "Защита от Точной атаки. Удача - Урон не проходит. Неудача - урон по заявленной атакующим точке, мимо брони(как правило)"
 		},
 		avoid: {
 			title: "Избежание",
-			group : "defence",
+			group: "defence",
 			description: "Защита от Площадной атаки. Удача - Урон не проходит. Неудача - Урон весь влетает по персонажу броне и эквипу."
 		},
-
 
 		evasion: {
 			title: "Уклонение",
 		},
 		hide: {
-			title:"Скрытность",
+			title: "Скрытность",
 		},
 		run: {
-			title:"Бег",
+			title: "Бег",
 		},
 		jump: {
 			title: "Прыжок",
