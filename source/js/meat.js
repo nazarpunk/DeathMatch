69fc3d7faf5e6bc7b779161b7c56b16264d3f281
nazarpunk <nazarpunk@ya.ru>
2017-02-05 18:25:20 +0300

    anim

--- source/js/meat.js
+++ source/js/meat.js
@@ -1,11 +1,23 @@
+window.rand = function(min, max) {
+	var rand = min - 0.5 + Math.random() * (max - min + 1);
+	rand = Math.round(rand);
+	return rand;
+};
+
 window.meat = {
-	spritetestDirection : function(dir){
+	spriteTestDirection : function(dir){
 		$('#meatUiDirectionTable td').removeClass('active');
 		$("#meatUiDirectionTable td[data-direction='"+dir+"']").addClass('active');
 		$('#meatuISpriteTestContainer .sprite').data('direction',dir).attr('data-direction',dir);
+	},
+	spriteTestAction : function(act){
+		$('#meatUiActionTable td').removeClass('active');
+		$("#meatUiActionTable td[data-action='"+act+"']").addClass('active');
+		$('#meatuISpriteTestContainer .sprite').data('action',act).attr('data-action',act);
 	}
 };
-window.meat.spritetestDirection('r');
+window.meat.spriteTestDirection(['t','tl','tr','l','r','b','bl','br'][window.rand(0,7)]);
+window.meat.spriteTestAction('stand');
 
 
 $('#meatUiDirectionTable').bind('click',function(e){
@@ -13,6 +25,15 @@ $('#meatUiDirectionTable').bind('click',function(e){
 	if (elem.length==0) return;
 	var d = elem.data('direction');
 	if (d==undefined) return;
-	window.meat.spritetestDirection(d);
+	window.meat.spriteTestDirection(d);
+	
+});
+
+$('#meatUiActionTable').bind('click',function(e){
+	var elem = $(e.target).closest('td');
+	if (elem.length==0) return;
+	var a = elem.data('action');
+	if (a==undefined) return;
+	window.meat.spriteTestAction(a);
 	
 });
